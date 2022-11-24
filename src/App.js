640f8e1d5866550b1032abda4aca18c9e2a25e6e
cxmedeiros <cxm@cin.ufpe.br>
2022-11-24 11:10:43 -0300

    change background with scroll

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 function App() {
 
   const container = useRef(null)
+  const [red, green, blue] = [14, 78, 255]
+  const section1 = document.querySelector('.section1')
+
 
   useEffect(() => {
     var animDuraction =  10000;
@@ -13,7 +16,7 @@ function App() {
       renderer: 'svg',
       loop: false,
       autoplay: false,
-      animationData: require('./cellphone.json')
+      animationData: require('./lottie.json')
     });
 
     function animatebodymovin(duration) {
@@ -33,25 +36,33 @@ function App() {
 
     document.addEventListener("scroll", onScroll);
 
+    window.addEventListener('scroll', () => {
+      const y = 1 + (window.scrollY || window.pageYOffset) / 250
+      const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+      section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    })
+
     return () => {
       anim.destroy();
       document.removeEventListener("scroll", onScroll);
     };
 
-  }, []);
+},);
 
 
   return (
-    
+    <div className="section1">
       <div className="App">
+      
         <text>
           <h1>A place in<br></br>the sun</h1>
           <p>Every summer, as 30 million people head to the<br></br> beautiful beaches of Italy, Spiagge is there to help <br></br>them find the perfect spot.</p>
 
           <p className= "text2"> Armed with new investments and ambitious plans for<br></br> the future, Italy’s “Airbnb of the beach” asked Belka <br></br>completely revamp their B2C mobile app for the new<br></br> season.</p>
         </text>
-      <div className='container' ref={container}></div>
+        <div className='container' ref={container}></div>
       </div>
+    </div>
  
   );
 }
